358ada6e355219e96ab51440734670a45d2e76d7
simonyang0701 <48277396+simonyang0701@users.noreply.github.com>
2019-03-19 19:19:00 -0400

    Add files via upload

---
+++ Strings Mix.java
@@ -0,0 +1,127 @@
+/*
+Given two strings s1 and s2, we want to visualize how different the two strings are. We will only take into account the lowercase letters (a to z). First let us count the frequency of each lowercase letters in s1 and s2.
+
+s1 = "A aaaa bb c"
+
+s2 = "& aaa bbb c d"
+
+s1 has 4 'a', 2 'b', 1 'c'
+
+s2 has 3 'a', 3 'b', 1 'c', 1 'd'
+
+So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. In the following we will not consider letters when the maximum of their occurrences is less than or equal to 1.
+
+We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4. In the same manner 2:bbb stands for string s2 and bbb because the maximum for b is 3.
+
+The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum if this maximum is strictly greater than 1; these letters will be prefixed by the number of the string where they appear with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
+
+In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; it contains the prefix) will be in decreasing order of their length and when they have the same length sorted in ascending lexicographic order (letters and digits - more precisely sorted by codepoint); the different groups will be separated by '/'. See examples and "Example Tests".
+
+Hopefully other examples can make this clearer.
+
+s1 = "my&friend&Paul has heavy hats! &"
+s2 = "my friend John has many many friends &"
+mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1="Are the kids at home? aaaaa fffff"
+s2="Yes they are here! aaaaa fffff"
+mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
+Note for Swift, R, PowerShell
+The prefix =: is replaced by E:
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+mix(s1, s2) --> "1:mmmmmm/E:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/E:ee/E:ss"
+https://www.codewars.com/kata/strings-mix/train/java/5c9028ca641c2c4d53c0f19d
+/*
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class Mixing {
+    //create new HashMap
+    static HashMap <Character, Integer> c1;
+    static HashMap <Character, Integer> c2;
+    public static String mix(String s1, String s2) {
+      // your code
+      List<Find> list = new ArrayList<Find>();
+        c1 = new HashMap <Character, Integer>();
+        c2 =  new HashMap <Character, Integer>();
+      build(s1,c1);
+      build(s2,c2);
+      for(char ch1 : c1.keySet()) {
+        if (c2.get(ch1) == null) {
+          if (c1.get(ch1) > 1)
+          list.add(new Find(c1.get(ch1), ch1, '1'));
+        } else {
+          char lar;
+          int count;
+          if (c1.get(ch1) > c2.get(ch1)) {
+            lar = '1';
+            count = c1.get(ch1);
+          } else if (c1.get(ch1) < c2.get(ch1)) {
+            lar = '2';
+            count = c2.get(ch1);
+          } else {
+            lar = '=';
+            count = c1.get(ch1);
+          }
+          if (count > 1)
+          list.add(new Find(count, ch1, lar));
+          c2.remove(ch1);
+        }
+      }
+      for(char ch1 : c2.keySet()) {
+          if (c2.get(ch1) > 1)
+          list.add(new Find(c2.get(ch1), ch1, '2'));
+      }
+      Collections.sort(list);
+      String s = "";
+      for (int i = 0; i < list.size(); i++) {
+        Find Find = list.get(i);
+        s += Find.print();
+        if (i != list.size() - 1)
+          s+="/";
+       }
+      return s;
+    }
+    //Build the new HashMap with the character and its number in the string
+    public static void build(String s1, HashMap <Character, Integer> c1) {
+      char[] array = s1.toCharArray();
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] >= 'a' && array[i] <= 'z') {
+          if (c1.get(array[i]) == null) {
+            c1.put(array[i], 1);
+          } else {
+            c1.put(array[i], 1 + c1.get(array[i]));
+          }
+        }
+      }
+    }
+    //Create new class Find to find the larger number of character in the string
+    public static class Find implements Comparable<Find>{
+      public int count;
+      public char ch;
+      public char larger;
+      public Find(int count, char ch,char larger) {
+        this.count = count;
+        this.ch = ch;
+        this.larger = larger;
+      }
+      public int compareTo(Find Find) {
+        return this.count == Find.count ? print().compareTo(Find.print()) : Find.count - this.count;
+      }
+      public String print() {
+        String s = "" + larger + ":";
+        for(int i = 0;i<count;i++) {
+          s+=ch;
+        }
+        return s;
+      }
+    }
+}
